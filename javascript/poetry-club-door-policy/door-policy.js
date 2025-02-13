73389012c0ba3a2b2f91bf9c037dc9f50b905d7c
Anthony Hamon <anthony.hamon91@gmail.com>
2025-02-13 17:11:22 +0100

    solved fifth exercise

--- javascript/poetry-club-door-policy/door-policy.js
+++ javascript/poetry-club-door-policy/door-policy.js
@@ -27,7 +27,7 @@
  * @returns {string}
  */
 export function frontDoorResponse(line) {
-  throw new Error('Remove this line and implement the function');
+  return line.charAt(0);
 }
 
 /**
@@ -38,7 +38,8 @@ export function frontDoorResponse(line) {
  * @returns {string} the front door password
  */
 export function frontDoorPassword(word) {
-  throw new Error('Remove this line and implement the function');
+  let formatedWord = word.charAt(0).toUpperCase() +  word.slice(1).toLowerCase()
+  return formatedWord;
 }
 
 /**
@@ -49,7 +50,8 @@ export function frontDoorPassword(word) {
  * @returns {string}
  */
 export function backDoorResponse(line) {
-  throw new Error('Remove this line and implement the function');
+  let formatedLine = line.trimEnd()
+  return formatedLine.charAt(formatedLine.length - 1);
 }
 
 /**
@@ -60,5 +62,6 @@ export function backDoorResponse(line) {
  * @returns {string} the back door password
  */
 export function backDoorPassword(word) {
-  throw new Error('Remove this line and implement the function');
+  let formatedWord = word.charAt(0).toUpperCase() +  word.slice(1).toLowerCase()
+  return formatedWord + ', please'
 }
