75c8cce8457cf8b95776d6ce9e9d52164615f836
Anthony Hamon <anthony.hamon91@gmail.com>
2025-02-16 09:47:28 +0100

    all tests passed

--- javascript/vehicle-purchase/test.js
+++ javascript/vehicle-purchase/test.js
@@ -1,7 +1,21 @@
+import { needsLicense, chooseVehicle, calculateResellPrice
 
+ } from "./vehicle-purchase.js"
 
 
 
 function testFunctions(){
-    
+    console.log(needsLicense("car"));
+    console.log(needsLicense("truck"));
+    console.log(needsLicense("bike"));
+    console.log(chooseVehicle("VW kaefer", "VW Ente"));
+    console.log(chooseVehicle("Audi A6", "Audi A4"));
+    console.log(chooseVehicle("Audi A6", "Opel"));
+    console.log(calculateResellPrice(20000, 1));
+    console.log(calculateResellPrice(20000, 2.5));
+    console.log(calculateResellPrice(20000, 5));
+    console.log(calculateResellPrice(20000, 10));
+
 }
+
+testFunctions()
