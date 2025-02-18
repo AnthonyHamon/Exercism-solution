011c183c0e9d6255d1ed8ca68be3b6f803145b91
Anthony Hamon <anthony.hamon91@gmail.com>
2025-02-18 19:39:57 +0100

    setted test function

---
+++ javascript/bird-watcher/test.js
@@ -0,0 +1,9 @@
+import { totalBirdCount } from "./bird-watcher.js"
+
+const birds = [0, 2, 5, 8, 9, 10, 4, 3, 2]
+
+function testFunctions(){
+    console.log(totalBirdCount(birds))
+}
+
+testFunctions();
