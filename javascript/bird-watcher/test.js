6e7e6342316dca5f398d7fd3fc8ae90527ce858e
Anthony Hamon <anthony.hamon91@gmail.com>
2025-02-20 18:07:51 +0100

    passed all tests

--- javascript/bird-watcher/test.js
+++ javascript/bird-watcher/test.js
@@ -1,9 +1,15 @@
-import { totalBirdCount } from "./bird-watcher.js"
+import { birdsInWeek, fixBirdCountLog, totalBirdCount } from "./bird-watcher.js"
 
-const birds = [0, 2, 5, 8, 9, 10, 4, 3, 2]
+const birds = [0, 2, 5, 8, 9, 10, 4, 3, 2];
+const birdsPerDay = [2, 5, 0, 7, 4, 1, 3, 0, 2, 5, 0, 1, 3, 1];
+const birdsPerDayToFix = [2, 5, 0, 7, 4, 1];
+
+
 
 function testFunctions(){
-    console.log(totalBirdCount(birds))
+    console.log(totalBirdCount(birds));
+    console.log(birdsInWeek(birdsPerDay, 2));
+    console.log(fixBirdCountLog(birdsPerDayToFix));
 }
 
 testFunctions();
