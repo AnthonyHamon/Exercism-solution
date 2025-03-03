11dd9d5ee0838c76045af12e383d28e26e7e4e53
Anthony Hamon <anthony.hamon91@gmail.com>
2025-03-03 19:00:22 +0100

    first function from 9th exercise working after test

---
+++ javascript/mixed-juices/test.js
@@ -0,0 +1,12 @@
+import { timeToMixJuice } from "./mixed-juices.js"; 
+
+const choosenJuice = 'Pure Strawberry Joy';
+
+
+
+
+function testFunction(){
+    console.log(timeToMixJuice(choosenJuice));
+}
+
+testFunction()
