e84975dfd9b2a590bb3565953eb15cfdf6bc2db0
Anthony Hamon <anthony.hamon91@gmail.com>
2025-03-04 19:18:53 +0100

    tests for seconc function successfully passed

--- javascript/mixed-juices/test.js
+++ javascript/mixed-juices/test.js
@@ -1,12 +1,24 @@
-import { timeToMixJuice } from "./mixed-juices.js"; 
+import { limesToCut, timeToMixJuice } from "./mixed-juices.js"; 
 
 const choosenJuice = 'Pure Strawberry Joy';
 
-
+const limes = [
+    'small',
+    'large',
+    'large',
+    'medium',
+    'small',
+    'large',
+    'large',
+  ];
 
 
 function testFunction(){
     console.log(timeToMixJuice(choosenJuice));
+    console.log(limesToCut(42, limes));
+    console.log(limesToCut(80, limes));
+    console.log(limesToCut(0, limes));
+    console.log(limesToCut(0, []));
 }
 
 testFunction()
