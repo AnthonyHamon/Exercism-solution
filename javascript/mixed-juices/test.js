2e015118acb7f2c2065525ef06997d121bb6edb7
Anthony Hamon <anthony.hamon91@gmail.com>
2025-03-09 10:33:38 +0100

    all test passed

--- javascript/mixed-juices/test.js
+++ javascript/mixed-juices/test.js
@@ -20,14 +20,14 @@ const limes = [
     'Pure Strawberry Joy',
   ];
 
-
 function testFunction(){
     console.log(timeToMixJuice(choosenJuice));
-    // console.log(limesToCut(42, limes));
-    // console.log(limesToCut(80, limes));
-    // console.log(limesToCut(0, limes));
-    // console.log(limesToCut(0, []));
-    // console.log(remainingOrders(7, orders));
+    console.log(limesToCut(42, limes));
+    console.log(limesToCut(4, ['medium', 'small']));
+    console.log(limesToCut(80, limes));
+    console.log(limesToCut(0, limes));
+    console.log(limesToCut(0, []));
+    console.log(remainingOrders(7, orders));
 }
 
 testFunction();
