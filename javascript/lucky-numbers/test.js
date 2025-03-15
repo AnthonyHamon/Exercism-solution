67e83c06ea69a7119fe0c46a4a6dac999016859c
Anthony Hamon <anthony.hamon91@gmail.com>
2025-03-15 22:34:06 +0100

    feat test for second function

--- javascript/lucky-numbers/test.js
+++ javascript/lucky-numbers/test.js
@@ -1,9 +1,10 @@
-import { twoSum } from "./lucky-numbers.js";
+import { luckyNumber, twoSum } from "./lucky-numbers.js";
 
 
 
 function testFunctions(){
-    console.log(twoSum([1, 2, 4, 0, 3, 5, 2, 9], [3, 2, 4, 8, 1, 5, 4, 1, 8]))
+    console.log(twoSum([1, 2, 4, 0, 3, 5, 2, 9], [3, 2, 4, 8, 1, 5, 4, 1, 8]));
+    console.logf(luckyNumber(15651));
 }
 
 testFunctions();
