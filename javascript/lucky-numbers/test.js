a2abfb87b95e91cd0746170e297fd93598a246e9
Anthony Hamon <anthony.hamon91@gmail.com>
2025-03-27 18:02:43 +0100

    last test for last function

--- javascript/lucky-numbers/test.js
+++ javascript/lucky-numbers/test.js
@@ -1,10 +1,11 @@
-import { luckyNumber, twoSum } from "./lucky-numbers.js";
+import { luckyNumber, twoSum, errorMessage } from "./lucky-numbers.js";
 
 
 
 function testFunctions(){
     console.log(twoSum([1, 2, 4, 0, 3, 5, 2, 9], [3, 2, 4, 8, 1, 5, 4, 1, 8]));
     console.log(luckyNumber(15651));
+    console.log(errorMessage('5e3'));
 }
 
 testFunctions();
