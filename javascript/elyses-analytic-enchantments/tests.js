019674be86a96542b7560b11fa94c0561fb67ca6
Anthony Hamon <anthony.hamon91@gmail.com>
2025-03-31 21:42:36 +0200

    wrote test for 11th exercise second function

--- javascript/elyses-analytic-enchantments/tests.js
+++ javascript/elyses-analytic-enchantments/tests.js
@@ -1,8 +1,10 @@
-import { getCardPosition } from "./enchantments.js"; 
+import { getCardPosition, doesStackIncludeCard } from "./enchantments.js"; 
 
 function testFunctions(){
     console.log(getCardPosition([1, 2, 3], 1));
     console.log(getCardPosition([1, 2, 3], 4));
+    console.log(doesStackIncludeCard([1, 2, 3], 1));
+    console.log(doesStackIncludeCard([1, 2, 3], 4));
 }   
 
 testFunctions()
