d7e9b50153fa94d439f561f1e2ddc145f7a190cb
Anthony Hamon <anthony.hamon91@gmail.com>
2025-04-18 21:19:43 +0200

    added console.logs for two tests

--- javascript/elyses-analytic-enchantments/tests.js
+++ javascript/elyses-analytic-enchantments/tests.js
@@ -1,10 +1,14 @@
-import { getCardPosition, doesStackIncludeCard } from "./enchantments.js"; 
+import { getCardPosition, doesStackIncludeCard, isEachCardEven, doesStackIncludeOddCard } from "./enchantments.js"; 
 
 function testFunctions(){
     console.log(getCardPosition([1, 2, 3], 1));
     console.log(getCardPosition([1, 2, 3], 4));
     console.log(doesStackIncludeCard([1, 2, 3], 1));
     console.log(doesStackIncludeCard([1, 2, 3], 4));
+    console.log(isEachCardEven([2, 4, 6]));
+    console.log(isEachCardEven([2, 5, 6]));
+    console.log(doesStackIncludeOddCard([2, 5]));
+    console.log(doesStackIncludeOddCard([2, 4, 6]));
 }   
 
 testFunctions()
